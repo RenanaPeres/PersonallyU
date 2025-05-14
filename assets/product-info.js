21719fa771305f3471abe39a2506377b1e348f8d
Nir Ellor <nirellorwaizner@gmail.com>
2025-05-14 17:22:51 +0300

    Remove incomplete console statement from resetProductFormState method

--- assets/product-info.js
+++ assets/product-info.js
@@ -80,7 +80,6 @@ if (!customElements.get('product-info')) {
       }
 
       resetProductFormState() {
-        console
         const productForm = this.productForm;
         productForm?.toggleSubmitButton(true);
         productForm?.handleErrorMessage();
