c42326aac26b0797544b8db88c89942d231e7a18
Nir Ellor <nirellorwaizner@gmail.com>
2025-05-28 20:13:54 +0300

    Update label padding and image width for improved display

--- assets/personalization.js
+++ assets/personalization.js
@@ -170,14 +170,14 @@ function getImageURLs() {
       if (label && !label.querySelector(`img[data-design="${id}"]`)) {
         // console.log("🔄 Updating label with image:", url);
         label.childNodes[0].textContent = '';
-        label.style.padding = "0px";
+        label.style.padding = "3px";
         label.style.borderRadius = "40px";
 
         const img = document.createElement("img");
         
         img.src = url;
         img.about = designedProductsURLsToPersonality[url] ?? "noPersonality";
-        img.width = 98;
+        img.width = 108;
         img.alt = "Design Image";
         img.dataset.design = id;
         img.style.borderRadius = "40px";
