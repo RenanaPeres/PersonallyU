25a0b86fb5ed0e1f20c1fd97d859c8689cad2881
Nir Ellor <nirellorwaizner@gmail.com>
2025-06-04 11:55:33 +0300

    Add debug log for mobile menu country currency integration

--- assets/personalization.js
+++ assets/personalization.js
@@ -515,6 +515,7 @@ if (window.innerWidth < 600) {
   const mobileMenuListClass = document.getElementsByClassName("menu-drawer__menu");
 
   if (countryCurrencyDivClass.length > 0 && mobileMenuListClass.length > 0) {
+    console.log("doing!!!");
     const countryCurrency = countryCurrencyDivClass[0];
     const mobileMenu = mobileMenuListClass[0];
 
