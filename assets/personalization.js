0d95a57aa8a8fa3396cfb64927c4fc45f831b455
Nir Ellor <nirellorwaizner@gmail.com>
2026-05-04 15:23:55 +0300

    label color

--- assets/personalization.js
+++ assets/personalization.js
@@ -394,6 +394,7 @@ function getImageURLs() {
         label.childNodes[0].textContent = '';
         label.style.padding = "3px";
         label.style.borderRadius = "25px";
+        label.color = "1B1818BF"
 
         const img = document.createElement("img");
         
