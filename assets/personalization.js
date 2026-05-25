3ca4fcadc7c8f1aa08b9097c0e751459ad4745a5
Nir Ellor <nirellorwaizner@gmail.com>
2026-05-25 22:41:58 +0300

    console.log(label.color);

--- assets/personalization.js
+++ assets/personalization.js
@@ -390,6 +390,7 @@ function getImageURLs() {
       const label = document.querySelector(`label[for="${id}"]`);
       // console.log("🔄 Label found:", label);
         label.color = "1B1818"
+        console.log(label.color);
       if (label && !label.querySelector(`img[data-design="${id}"]`)) {
         console.log("🔄 Updating label with image:", url);
         label.childNodes[0].textContent = '';
